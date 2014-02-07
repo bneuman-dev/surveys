ec49539358e3dbb6763b3f710309e20f3a17cc8f
Apprentice <apprentice@dbc-chicago-14.local>
2014-02-07 14:14:19 -0600

    survey create form and submit to database successful

--- public/js/application.js
+++ public/js/application.js
@@ -5,23 +5,48 @@ $(document).ready(function() {
   var $question = "";
   for(questionCount=0;questionCount<startingNo;questionCount++){
       var displayCount = questionCount+1;
-      $question += '<p><label for="question'+displayCount+'">Question '+displayCount+': </label><input type="text" name="question'+displayCount+'" id="question'+displayCount+'"><span class="removeQuestion">Remove Question</span><br><label for="answers'+displayCount+'">Answers: </label><textarea cols="37" rows="5" name="answers'+displayCount+'"></textarea></p>';
+      $question += '<p><label for="question'+displayCount+'">Question '+displayCount+': </label><input type="text" name="question['+displayCount+']" id="question'+displayCount+'" class="required"><button class="removeQuestion">Remove Question</button><br><label for="answers'+displayCount+'">Answers: </label><textarea cols="37" rows="5" name="answer['+displayCount+']"></textarea></p>';
   }
 //add them to the DOM
-  $('form').prepend($question);
+  $('.question').prepend($question);
 //remove a textfield
-  $('form').on('click', '.removeQuestion', function(){
+  $('form').on('click', '.removeQuestion', function(e){
+     e.preventDefault();
      $(this).parent().remove();
 
   });
   //add a new node
-  $('#addQuestion').on('click', function(){
+  $('#addQuestion').on('click', function(e){
+    e.preventDefault();
     questionCount++;
-    $question = '<p><label for="question'+questionCount+'">Question '+questionCount+': </label><input type="text" name="question'+questionCount+'" id="question'+questionCount+'"><span class="removeQuestion">Remove Question</span><br><label for="answers'+questionCount+'">Answers: </label><textarea cols="37" rows="5" name="answers'+questionCount+'"></textarea></p>';
+    $question = '<p><label for="question'+questionCount+'">Question '+questionCount+': </label><input type="text" name="question['+questionCount+']" id="question'+questionCount+'" class="required"><button class="removeQuestion">Remove Question</button><br><label for="answers'+questionCount+'">Answers: </label><textarea cols="37" rows="5" name="answer['+questionCount+']"></textarea></p>';
     $(this).parent().before($question);
   });
 
 
 
+  // $('form').on('submit', function(e){
+  //   if(check_form()===false){
+  //     e.preventDefault();
+  //   }
+
+  // });
+
+
+
+
+
 
 });
+
+//   var check_form = function(){
+//   console.log("something happening");
+//   var flag = true;
+//   $('form .required').each(function(){
+//     if ($(this)).val()===""){
+//       show_dialog('Please enter a value for ' + $(this).attr('name'));
+//       flag = false;
+//     }
+//   });
+//   return flag;
+// }
