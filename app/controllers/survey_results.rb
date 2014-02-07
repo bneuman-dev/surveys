7d1ed6ae41d52f81163b433c21aafe0074618be8
Mike Busch & Ryan Carmelo Briones <webblewebble@yahoo.com>
2014-02-07 16:18:27 -0600

    Implemented logic for user_responses route to display responses to survey chunked by user; implemented basic view

--- app/controllers/survey_results.rb
+++ app/controllers/survey_results.rb
@@ -1,20 +1,6 @@
 
 get '/surveys/:survey_id/results' do
 
-  #Displays 'question summaries'
-    #For each survey question we see:
-    #A table that breaks down percentage of respondents who picked each choice
-    #A graphical representation of the table
-    #Feature: maybe for open-ended questions, just list all the answers given
-
-    #Look up all questions with survey_id
-    #For each question:
-      #Look up all possible_answers with question_id
-      #Then, look up all responses with possible_answer_id
-
-  #MAYBE FEATURE: Links to 'individual responses', which chunks responses by user ->
-    #sends us to /surveys/:survey_id/responses
-
   @survey = Survey.find(params[:survey_id])
   @questions = @survey.questions
   @question_stats = @questions.map do |q|
@@ -22,21 +8,32 @@ get '/surveys/:survey_id/results' do
   end
 
  @question_stats
- # @question_stats.each do |qs|
- #  Question: qs[:question]
- #    qs[:stats].each do |s|
- #      Answer: s[:answer]
- #      Count: s[:count]
- #    end
- #  end
-
  erb :survey_results
 end
 
 
+get '/user_responses/:response_id' do
+  user_survey = UserSurvey.find(params[:response_id])
+  @user = user_survey.user
+  @survey = user_survey.survey
+  questions = @survey.questions
+  @answers = questions.collect do |question|
+    poss = question.possible_answer_ids
+    response = Response.where(user_id: @user.id, possible_answer_id: poss).first
+
+    {question: question.question, response: response.possible_answer.text}
+  end
+
+  erb :user_responses
+
+end
 
 
-
+#From survey results:
+  #Feature: maybe for open-ended questions, just list all the answers given
+  #MAYBE FEATURE: Links to 'individual responses', which chunks responses by user ->
+    #sends us to /surveys/:survey_id/responses
+  #REALLY MAYBE FEATURE: #A graphical representation of the table
 
 
 
@@ -56,6 +53,7 @@ end
 
 
 get '/surveys/:survey_id/responses' do
+
    #THIS FEATURE LOOKS LIKE WORK. Part of SurveyMonkey but not part of MVP
   #NOTE: WHEN WE SAY 'responses' here we mean 'user_surveys' in the models
 
@@ -71,7 +69,7 @@ get '/surveys/:survey_id/responses' do
       #/surveys/:survey_id/responses/:response_id/delete
 end
 
-get '/surveys/:survey_id/responses/:response_id' do
+
   #THIS FEATURE LOOKS LIKE WORK. Part of SurveyMonkey but not part of MVP
 
   #This should list:
@@ -88,4 +86,4 @@ get '/surveys/:survey_id/responses/:response_id' do
 
   #We can look this up by:
     #Looking up
-end
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,22 @@
 require 'faker'
 
-# create a few users
-user = User.create!(name:"user1", email: "email@email.com", password: 'password')
+# # create a few users
+# user = User.create!(name:"user1", email: "email@email.com", password: 'password')
 
-10.times do
-  user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+# 10.times do
+#   user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
 
-  (rand(5)).times do
-    survey = user.surveys.create(title: Faker::Commerce.product_name, description: Faker::Company.catch_phrase, pic_url: Faker::Company.catch_phrase)
-    (rand(10)).times do
-      question = survey.questions.create(question: Faker::Company.bs)
-      (rand(6) + 2).times do |n|
-        question.possible_answers.create(text: Faker::Company.catch_phrase)
-      end
-    end
-  end
-end
+#   (rand(5)).times do
+#     survey = user.surveys.create(title: Faker::Commerce.product_name, description: Faker::Company.catch_phrase, pic_url: Faker::Company.catch_phrase)
+#     (rand(10)).times do
+#       question = survey.questions.create(question: Faker::Company.bs)
+#       (rand(6) + 2).times do |n|
+#         question.possible_answers.create(text: Faker::Company.catch_phrase)
+#       end
+#     end
+#   end
+# end
+
 
 Survey.all.each do |s|
   5.times do
