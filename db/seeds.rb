a6baaf2e2771ebba27fea316db2353f1787ba240
gnitnuj <junting.lu@gmail.com>
2014-02-07 14:34:24 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,18 @@
-# Create two new users
-User.create(name: 'charlie', email: 'charlie@charlie.com', password: '123456')
+require 'faker'
 
-User.create(name: 'laura', email: 'laura@laura.com', password: '123456')
+# create a few users
+user = User.create!(name:"user1", email: "email@email.com", password: 'password')
 
-# Create dummy surveys for Laura and Charlie.
-5.times do
-
-  Survey.create(title: 'laura_title', description: 'laura_description', pic_url: 'laura_pictureurl', creator_id:2)
-
-end
-
-15.times do
-
-  Survey.create(title: 'charlie_title', description: 'charlie_description', pic_url: 'charlie_pictureurl', creator_id:1)
-
-end
-
-# 5 questions on Charlie survey, id 1.
-5.times do
-
-  Question.create(question: 'charlie_question', survey_id: 1)
-
-end
-
-# 10 questions on Laura survey, id 6.
 10.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
 
-  Question.create(question: 'laura_question', survey_id: 6)
-
+  (rand(5)).times do
+    survey = user.surveys.create(title: Faker::Commerce.product_name, description: Faker::Company.catch_phrase, pic_url: Faker::Company.catch_phrase)
+    (rand(10)).times do
+      question = survey.questions.create(question: Faker::Company.bs)
+      (rand(6) + 2).times do |n|
+        question.possible_answers.create(text: Faker::Company.catch_phrase)
+      end
+    end
+  end
 end
-
-# 5 answers on Charlie survey 1, question 1.
-5.times do
-
-  PossibleAnswer.create(text: 'charlie_answer', question_id: 1)
-
-end
-
-# 10 answers on Laura survey 6, question 6.
-10.times do
-
-  PossibleAnswer.create(text: 'laura_answer', question_id: 6)
-
-end
-
-# Create User and answer a question on a survey.
-User.create(name: 'junting', email: 'junting@junting.com', password: '123456')
-# Pick possible answer id 10 on laura survey 6, quesiton 6.
-Response.create(user_id: 3, possible_answer_id: 10)
