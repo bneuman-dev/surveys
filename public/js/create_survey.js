c1c26798ae79889763e56a146fa1a3a16d0126a9
Apprentice <apprentice@dbc-chicago-14.local>
2014-02-07 15:39:01 -0600

    implemented .js validation on create survey form

---
+++ public/js/create_survey.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+//create three initial fields
+  var startingNo = 1;
+  var $question = "";
+  for(questionCount=0;questionCount<startingNo;questionCount++){
+      var displayCount = questionCount+1;
+      $question += '<p><label for="question'+displayCount+'">Question '+displayCount+': </label><input type="text" name="question['+displayCount+']" id="question'+displayCount+'" class="required"><button class="removeQuestion">Remove Question</button><br><label for="answers'+displayCount+'">Answers: </label><textarea cols="37" rows="5" name="answer['+displayCount+']" class="required"></textarea></p>';
+  }
+//add them to the DOM
+  $('.question').prepend($question);
+//remove a textfield
+  $('form').on('click', '.removeQuestion', function(e){
+     e.preventDefault();
+     $(this).parent().remove();
+
+  });
+  //add a new node
+  $('#addQuestion').on('click', function(e){
+    e.preventDefault();
+    questionCount++;
+    $question = '<p><label for="question'+questionCount+'">Question '+questionCount+': </label><input type="text" name="question['+questionCount+']" id="question'+questionCount+'" class="required"><button class="removeQuestion">Remove Question</button><br><label for="answers'+questionCount+'">Answers: </label><textarea cols="37" rows="5" name="answer['+questionCount+']" class="required"></textarea></p>';
+    $(this).parent().before($question);
+  });
+
+
+
+  $('form').on('submit', function(e){
+    if(check_form()===false){
+      e.preventDefault();
+    }
+
+  });
+
+
+
+
+
+
+});
+
+// var check_form = function(){
+//   var title = document.forms["create"]["title"].value;
+//   if (title==null || title==""){
+//     alert("Your form must include a title.");
+//     return false;
+//   }
+// }
+
+var check_form = function(){
+  $('form .required').each(function(){
+    if ((this).value===null || (this).value===""){
+      alert("You must fill out all title, question, and answer fields to submit your survey.");
+      return false;
+    }
+  });
+}
+
+//   var check_form = function(){
+//   console.log("something happening");
+//   var flag = true;
+//   $('form .required').each(function(){
+//     if ($(this)).val()===""){
+//       show_dialog('Please enter a value for ' + $(this).attr('name'));
+//       flag = false;
+//     }
+//   });
+//   return flag;
+// }
